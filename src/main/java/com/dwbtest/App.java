1eeb286a741f21cfcdd474836e238c974260837a
Don Brinker <don.brinker@appian.com>
2015-08-31 15:02:32 -0400

    Corrected ONE issue

--- src/main/java/com/dwbtest/App.java
+++ src/main/java/com/dwbtest/App.java
@@ -4,8 +4,7 @@ package com.dwbtest;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
